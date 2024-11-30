1a5e5164f5c1eadbf8606e9602a1613dcbd7ae13
DJ Cartwright <dj.cartwright.web@gmail.com>
2024-11-30 10:56:25 -0500

    Initial Commit

---
+++ SarniaLocations/SarniaLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SarniaLocationsApp.swift
+//  SarniaLocations
+//
+//  Created by David Cartwright on 2024-11-30.
+//
+
+import SwiftUI
+
+@main
+struct SarniaLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
